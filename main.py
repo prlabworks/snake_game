439ac67db5dc0e07d659fa174ab9455f6ce8f986
Peter Rackley <prlabwork@gmail.com>
2025-08-29 10:13:11 -0400

    Update import to remove Turtle and also removed commented/refactored coed

--- main.py
+++ main.py
@@ -1,27 +1,17 @@
 # classic game of Snake
 
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
 import time
 
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=600, height=600, startx=650, starty=300)
 screen.bgcolor("black")
 screen.title("Snake")
 screen.tracer(0)  # Turn off automatic screen updates
 
-# starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-# segments = []
-# for position in starting_positions:
-#     new_segment = Turtle("square")
-#     new_segment.color("white")
-#     new_segment.penup()
-#     new_segment.goto(position)
-#     segments.append(new_segment)
-
 my_snake = Snake()
 segments = my_snake.segments 
-
 
 game_is_on = True
 
