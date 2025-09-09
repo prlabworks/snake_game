7e778e10ee73d7afd76830a9a7cbf3e5b49caad1
Peter Rackley <prlabwork@gmail.com>
2025-09-09 13:51:59 -0400

    Commented main.py

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ from turtle import Screen
 from snake import Snake
 import time
 
+# Set up the screen for the game with specified dimensions and background color
 screen = Screen()
 screen.setup(width=600, height=600, startx=650, starty=300)
-
 screen.bgcolor("black")
 screen.title("Snake")
 screen.tracer(0)  # Turn off automatic screen updates
 
 snake = Snake()
 
+# Set up key bindings for controlling the snake
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -21,8 +22,10 @@ screen.onkey(snake.right, "Right")
 
 segments = snake.segments 
 
+# Initialize the game loop control variable
 game_is_on = True
 
+# Main game loop to keep the game running and update the screen
 while game_is_on:
     
     screen.update()
