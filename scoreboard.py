1915c3a5e2b5af0438c47579fe56e6765a815808
Peter Rackley <prlabwork@gmail.com>
2025-09-24 12:59:44 -0400

    Added save high score to data.txt

--- scoreboard.py
+++ scoreboard.py
@@ -9,7 +9,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.read_high_score() # Initialize high score from file
         self.penup()
         self.color("white")
         self.hideturtle()
@@ -27,7 +27,18 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.write_high_score()
         self.score = 0
         self.update_scoreboard()
        
+    def read_high_score(self):
+        try:
+            with open("data.txt", "r") as file:
+                return int(file.read())
+        except (FileNotFoundError, ValueError):
+            return 0
+            
+    def write_high_score(self):
+        with open("data.txt", "w") as file:
+            file.write(str(self.high_score))
     
